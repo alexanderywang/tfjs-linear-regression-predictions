693d2054a35131a64a3c360af668b181f9c7b0b9
Alexander Wang <alexander.wang22@gmail.com>
2021-02-26 11:01:36 -0500

    data filtered and cleaned up

--- src/Api.js
+++ src/Api.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const Api = () => {
+const Api = ({ filter }) => {
   const URL = "https://storage.googleapis.com/tfjs-tutorials/carsData.json";
   /*********
    * SHAPE OF THE DATA FOR REFERENCE:
-   * arrays of 100 car objects
+   * array of 406 car objects
    * each object has {Acceleration, Cylinders, Displacement, Horsepower, Miles_perGallon, Name, Origin, Weight_in_lbs, Year }
    * Name, Oriign, Year are strings. rest are numbers
    */
@@ -13,23 +13,30 @@ const Api = () => {
 
   const getData = async () => {
     const { data } = await axios(URL);
-    // console.log(data);
+    console.log(data);
     setCarInfo(data);
+    filter(data);
   };
 
   return (
     <div>
       <button onClick={getData}>Get Data</button>
-      <h4>An example of a car object data:</h4>
-      <h6>Acceleration: {carInfo[0].Acceleration}</h6>
-      <h6>xCylinders: {carInfo[0].xCylinders}</h6>
-      <h6>Displacement: {carInfo[0].Displacement}</h6>
-      <h6>Horsepower: {carInfo[0].Horsepower}</h6>
-      <h6>Miles_perGallon: {carInfo[0].Miles_perGallon}</h6>
-      <h6>Name: {carInfo[0].Name}</h6>
-      <h6>Origin: {carInfo[0].Origin}</h6>
-      <h6>Weight_in_lbs: {carInfo[0].Weight_in_lbs}</h6>
-      <h6>Year: {carInfo[0].Year}</h6>
+      {carInfo.length ? (
+        <>
+          <h4>An example of a car object data:</h4>
+          <h6>Acceleration: {carInfo[0].Acceleration}</h6>
+          <h6>Cylinders: {carInfo[0].Cylinders}</h6>
+          <h6>Displacement: {carInfo[0].Displacement}</h6>
+          <h6>Horsepower: {carInfo[0].Horsepower}</h6>
+          <h6>Miles_per_Gallon: {carInfo[0].Miles_per_Gallon}</h6>
+          <h6>Name: {carInfo[0].Name}</h6>
+          <h6>Origin: {carInfo[0].Origin}</h6>
+          <h6>Weight_in_lbs: {carInfo[0].Weight_in_lbs}</h6>
+          <h6>Year: {carInfo[0].Year}</h6>
+        </>
+      ) : (
+        <h6>loading...</h6>
+      )}
     </div>
   );
 };
--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Api from "./Api";
 
 function App() {
+  const [filteredData, setFilteredData] = useState([]);
+
+  const filterData = data => {
+    const filtered = data.map(car => ({
+      mpg: car.Miles_per_Gallon,
+      horsepower: car.Horsepower
+    }));
+    // console.log("filtered data", filtered);
+    setFilteredData(filtered);
+  };
+  
   return (
     <div className="App">
       Hello TensorFlow
-      <Api />
+      <Api filter={filterData} />
     </div>
   );
 }
